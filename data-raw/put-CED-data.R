766356efbc36586c2c64115c0ae22b0a9f29156e
HughParsonage <hugh.parsonage@gmail.com>
2022-06-29 17:06:44 +1000

    Initial move to 2021

--- data-raw/put-CED-data.R
+++ data-raw/put-CED-data.R
@@ -9,8 +9,8 @@ print_tot <- function(DT) {
 }
 
 UsualResidence_by_Age <-
-  fread("./data-raw/data-packs/data/CED/AUST/2016Census_G03_AUS_CED.csv") %>%
-  melt.data.table(id.vars = "CED_CODE_2016", variable.factor = FALSE) %>%
+  fread("./data-raw/data-packs/data/CED/AUST/2021Census_G03_AUS_CED.csv") %>%
+  melt.data.table(id.vars = "CED_CODE_2021", variable.factor = FALSE) %>%
   .[!(variable %pin% "Tot")] %>%
   .[variable %pin% "(yr|85ov)$"] %>%
   .[, Age := gsub("^.*?_([0-9]+(?:_[0-9]{2})?(?:ov)?)(?:_yr)?$", "\\1", variable, perl = TRUE)] %>%
@@ -23,9 +23,9 @@ UsualResidence_by_Age <-
     UsualResidence := paste("Visitor different SA2:", gsub("VisDiff_SA2_", "", ResidenceVar))] %>%
   .[ResidenceVar %pin% "VisSame",
     UsualResidence := "Visitor same SA2"] %>%
-  .[, .(CED_CODE_2016, Age, UsualResidence, value)] %>%
-  setkey(CED_CODE_2016, Age, UsualResidence) %>%
+  .[, .(CED_CODE_2021, Age, UsualResidence, value)] %>%
+  setkey(CED_CODE_2021, Age, UsualResidence) %>%
   .[]
 
-fread("./data-raw/data-packs/data/CED/AUST/2016Census_G01_AUS_CED.csv")
-fread("./data-raw/data-packs/data/CED/AUST/2016Census_G02_AUS_CED.csv")
+fread("./data-raw/data-packs/data/CED/AUST/2021Census_G01_AUS_CED.csv")
+fread("./data-raw/data-packs/data/CED/AUST/2021Census_G02_AUS_CED.csv")
--- data-raw/put-SA16_decoder.R
+++ data-raw/put-SA16_decoder.R
@@ -3,9 +3,10 @@ library(magrittr)
 library(readxl)
 
 SA16_decoder <-
-  read_excel("./data-raw/data-packs/Metadata/2016Census_geog_desc_1st_release.xlsx",
+  read_excel("./data-raw/data-packs/Metadata/2021Census_geog_desc_1st_release.xlsx",
              sheet = 1) %>%
   as.data.table %>%
-  .[ASGS_Structure != "SA1", .(ASGS_Structure, Census_Code_2016 = as.integer(Census_Code_2016), Census_Name_2016)]
+  .[ASGS_Structure != "SA1", .(ASGS_Structure, Census_Code_2021 = as.integer(Census_Code_2021), Census_Name_2021)] %>%
+  fwrite("data-raw/SA21_decoder.csv")
 
 devtools::use_data(SA16_decoder, overwrite = TRUE)
