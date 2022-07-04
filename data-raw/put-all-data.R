ebdcc1e64bfd7c2c79b3258f716eab3c21f5af17
HughParsonage <hugh.parsonage@gmail.com>
2022-07-04 22:48:32 +1000

    Add git for loop

--- data-raw/put-all-data.R
+++ data-raw/put-all-data.R
@@ -12,3 +12,15 @@ for (Region in c("STE", "SED",
 }
 
 source("data-raw/put-SA16_decoder.R")
+
+for (Region in c("STE", "SED",
+                 # "SA1",
+                 "SA2", "SA3", "SA4", "LGA", "CED",
+                 # "SSC",
+                 "POA")) {
+  shell('git config --global user.email "hugh.parsonage@gmail.com"')
+  shell('git config --global user.name "HughParsonage"')
+  shell(paste0("git add ./tsv/", Region, "*tsv"))
+  shell(paste0("git commit -m ", '"Add ', Region, '"'))
+  shell("git push")
+}
